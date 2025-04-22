93ba9f10e37e4d0034924475df38eb59fe1027cb
Arnav Surve <arnav@surve.dev>
2025-04-22 11:51:18 -0700

    cli scaffold

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var outDir string
+
+var rootCmd = &cobra.Command{
+	Use:   "grace",
+	Short: "Grace is a modern IaC and COBOL developer experience",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Grace CLI: mainframe automation, reborn.")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
